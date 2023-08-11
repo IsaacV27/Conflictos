7fe8daf416e04c0a8c7cc85a6ef58c1b5a553b42
IsaacVargasV <isafa2727@gmail.com>
2023-08-11 16:09:14 -0600

    Se crea la clase persona

--- src/conflictos/Conflictos.java
+++ src/conflictos/Conflictos.java
@@ -14,7 +14,9 @@ public class Conflictos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+    int edad;
+    int nombre;
+    
     }
     
 }
